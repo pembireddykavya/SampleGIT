1a0957336809361613cfab2d38dc30a1e7022105
Yogesh Rajput <yogeshrajput335@gmail.com>
2024-08-08 23:00:08 +0530

    component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyHeaderComponent } from './pages/my-header/my-header.component';
+import { MyBodyComponent } from './pages/my-body/my-body.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MyHeaderComponent,MyBodyComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  
 })
 export class AppComponent {
   title = 'my-first-project';
---
+++ src/app/pages/my-body/my-body.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-my-body',
+    template:'<div style="width:100%;background-color: blanchedalmond;height: 400px;">BODY</div>',
+    standalone:true
+})
+export class MyBodyComponent{
+
+}
